70f1bf2bbe217a66f2a2f26c9b97029f0371e901
Raushan225024 <raushankumar225024@gmail.com>
2026-08-14 23:58:28 +0530

    configer store in the main.jsx

--- frontend/agent_ui/src/main.jsx
+++ frontend/agent_ui/src/main.jsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {provider} from 'react-redux'
 import './index.css'
 import App from './App.jsx'
+import { store } from './redux/store.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>,
+)
+
+    
     <App />
   </StrictMode>,
 )
